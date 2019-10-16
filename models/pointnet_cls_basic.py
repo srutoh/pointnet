67dad2f15958cb43f5e30a1a3875f128e8b09c9b
Sihong Chen <sihong.chen@tum.de>
2019-10-16 22:37:54 +0200

    add num_class argument

--- models/pointnet_cls_basic.py
+++ models/pointnet_cls_basic.py
@@ -14,8 +14,8 @@ def placeholder_inputs(batch_size, num_point):
     return pointclouds_pl, labels_pl
 
 
-def get_model(point_cloud, is_training, bn_decay=None):
-    """ Classification PointNet, input is BxNx3, output Bx40 """
+def get_model(point_cloud, is_training, bn_decay=None, num_class=51):
+    """ Classification PointNet, input is BxNx3, output BxK, (K=num_class) """
     batch_size = point_cloud.get_shape()[0].value
     num_point = point_cloud.get_shape()[1].value
     end_points = {}
@@ -55,7 +55,7 @@ def get_model(point_cloud, is_training, bn_decay=None):
                                   scope='fc2', bn_decay=bn_decay)
     net = tf_util.dropout(net, keep_prob=0.7, is_training=is_training,
                           scope='dp1')
-    net = tf_util.fully_connected(net, 40, activation_fn=None, scope='fc3')
+    net = tf_util.fully_connected(net, num_class, activation_fn=None, scope='fc3')
 
     return net, end_points
 
