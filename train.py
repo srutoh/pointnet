c0bfd16dc73fbe6d0763f23416d92eead82a403c
Sihong Chen <sihong.chen@tum.de>
2019-10-19 03:17:34 +0200

    fix time

--- train.py
+++ train.py
@@ -8,6 +8,7 @@ import socket
 import importlib
 import os
 import sys
+import time
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(BASE_DIR)
 sys.path.append(os.path.join(BASE_DIR, 'models'))
